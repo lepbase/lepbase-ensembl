bbec0bee2c4583b5e55424e692e4699b643e9916
Richard Challis <rjchallis@gmail.com>
2015-09-10 10:25:21 +0100

    test autocomplete

--- htdocs/components/08_searchbox.js
+++ htdocs/components/08_searchbox.js
@@ -2,9 +2,9 @@ window.onload = function(){
     $(function() {
           $("#se_q").autocomplete({
                 source: function(request, response) {
-    				console.log(request)
-    				//$.getJSON("/cgi-bin/autocomplete.pl", { term: request.term, table: $('#table').val() }, 
-              		//response);
+    				console.log(request);
+    				$.getJSON("autocomplete.pl", { term: request.term }, 
+              		response);
   				},
                 minLength: 3,
                 select: function(event, ui) {
