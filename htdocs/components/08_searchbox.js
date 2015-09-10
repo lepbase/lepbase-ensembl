3de5aa3cbc412e7c4abacee6f0575d168ae5b2a8
Richard Challis <rjchallis@gmail.com>
2015-09-10 11:32:03 +0100

    send all autocomplete requests to the same absolute url

--- htdocs/components/08_searchbox.js
+++ htdocs/components/08_searchbox.js
@@ -2,8 +2,13 @@ window.onload = function(){
     $(function() {
           $("#se_q").autocomplete({
                 source: function(request, response) {
-    				console.log(request.term + ' ' + $('#search_table').val());
-    				$.getJSON("/autocomplete", { term: request.term, table: $('#search_table').val() }, 
+    				var url = window.location.pathname.split('/');
+    				url.pop();
+    				var prefix = '';
+    				url.forEach(function(i){
+    					prefix .= '../';
+    				});
+    				$.getJSON(prefix+"autocomplete", { term: request.term, table: $('#search_table').val() }, 
               		response);
   				},
                 minLength: 3,
