264f65f2b10f7fb21c17b76734047e7e2abf930b
Richard Challis <rjchallis@gmail.com>
2015-09-10 11:57:50 +0100

    define prefix

--- htdocs/components/08_searchbox.js
+++ htdocs/components/08_searchbox.js
@@ -1,13 +1,13 @@
 window.onload = function(){
     $(function() {
-          $("#se_q").autocomplete({
-                source: function(request, response) {
-    				var url = window.location.pathname.split('/');
+    	var url = window.location.pathname.split('/');
     				url.pop();
     				var prefix = '';
     				url.forEach(function(i){
     					prefix += '../';
     				});
+          $("#se_q").autocomplete({
+                source: function(request, response) {
     				$.getJSON(prefix+"autocomplete", { term: request.term, table: $('#search_table').val() }, 
               		response);
   				},
