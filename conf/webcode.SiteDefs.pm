2f302b4b36914c2a88318d6b3f726b59763153d2
Richard Challis <rjchallis@gmail.com>
2015-09-18 15:00:04 +0100

    minify js

--- conf/webcode.SiteDefs.pm
+++ conf/webcode.SiteDefs.pm
@@ -168,7 +168,7 @@ our $ENSEMBL_GROUP             = getgrgid($)); # Auto-set web server group
 our $ENSEMBL_IMAGE_WIDTH       = 800;
 our $ENSEMBL_JSCSS_TYPE        = 'minified';
 our $ENSEMBL_MINIFIED_FILES_PATH = '/minified'; # path for saving the minified files
-our $ENSEMBL_DEBUG_JS            = 1; # change these to 1 to prevent js minification
+our $ENSEMBL_DEBUG_JS            = 0; # change these to 1 to prevent js minification
 our $ENSEMBL_DEBUG_CSS           = 0; # change these to 1 to prevent css minification
 
 our $ENSEMBL_EXTERNAL_SEARCHABLE = 0; # No external bots allowed by default
