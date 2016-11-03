a8fc9ece4f8d8ea50fcebc2e026ce2fc4023b1d3
Richard Challis <rjchallis@gmail.com>
2016-11-03 14:34:41 +0000

    remove unused code

--- modules/EnsEMBL/Web/Document/Element/Tabs.pm
+++ modules/EnsEMBL/Web/Document/Element/Tabs.pm
@@ -23,29 +23,15 @@ package EnsEMBL::Web::Document::Element::Tabs;
 
 ## BEGIN LEPBASE MODIFICATIONS...
 
-sub old_species_list {
-  my $self      = shift;
-
-  my $html;
-  foreach my $sp (@{$self->{'species_list'}}) {
-## BEGIN LEPBASE MODIFICATIONS...
-    $html .= qq{<li><a class="constant" href="$sp->[0]">$sp->[2]</a></li>} unless $sp->[1] =~ m/helico[12]$/;
-## ..END LEPBASE MODIFICATIONS
-  }
-  
-  return qq{<div class="dropdown species"><h4>Select a species</h4><ul>$html</ul></div>};
-}
-
-
 
 sub species_list {
   my $self = shift;
-  
+
   my $hub           = $self->hub;
   my $species_info  = $hub->get_species_info;
-  
+
   my (@ok_faves, %check_faves);
-  
+
   foreach (@{$hub->get_favourite_species}) {
     push @ok_faves, [$species_info->{$_}->{'url'},$species_info->{$_}->{'scientific'}] unless $check_faves{$species_info->{$_}->{'scientific'}}++;
   }
@@ -53,7 +39,7 @@ sub species_list {
   foreach my $sp (@ok_faves) {
     $html .= qq{<li><a class="constant" href="$sp->[0]">$sp->[1]</a></li>};
   }
-  
+
   return qq{<div class="dropdown species"><h4>Select a species</h4><ul>$html</ul></div>};
 }
 ## ..END LEPBASE MODIFICATIONS
